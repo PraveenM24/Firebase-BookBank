675c4eb74d6c1b79ea639d08008dff3da6b35b54
PraveenM24 <praveen050800@gmail.com>
2021-03-08 23:20:42 +0530

    #3

--- public/app.js
+++ public/app.js
@@ -1,24 +1,16 @@
-function redirect(){
-  window.location.href='CSE/CSE1.html';
-  var dep = document.getElementById('department').value;
-  var year = document.getElementById('year').value;
-  if (dep == 'cse'){
-    if (year == '1y'){
-      window.location.href='CSE/CSE1.html';
+function redirect() {
+    var dep = document.getElementById('department').value;
+    var year = document.getElementById('year').value;
+    if (dep == 'cse') {
+        if (year == '1y') {
+            window.location.href = './CSE/CSE1.html';
+        } else if (year == '2y') {
+            window.location.href = './CSE/CSE2.html';
+        } else if (year == '3y') {
+            window.location.href = './CSE/CSE3.html';
+        } else if (year == '4y') {
+            window.location.href = './CSE/CSE4.html';
+        }
     }
-    else if (year == '2y') {
-      window.location.href='CSE/CSE2.html';
-    }
-    else if (year == '3y') {
-      window.location.href='CSE/CSE3.html';
-    }
-    else if (year == '4y') {
-      window.location.href='CSE/CSE4.html';
-    }
-  }
-  return false;
-}
-
-function test() {
-  window.location.href="CSE/CSE1.html";
+    return false;
 }
